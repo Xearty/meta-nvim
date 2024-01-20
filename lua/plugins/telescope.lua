5faaacdc3a33752ae45d369ec7f5ad1d569cb699
Xearty <xeartybg@gmail.com>
2024-01-20 18:26:08 +0200

    feat(telescope): add keymappings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,4 +2,25 @@ return {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.5',
     dependencies = { 'nvim-lua/plenary.nvim' },
+    keys = {
+        {
+            "<leader>gb",
+            function()
+                require("telescope.builtin").git_branches()
+            end,
+        },
+    },
+    config = function()
+        local actions = require("telescope.actions")
+        require("telescope").setup{
+            defaults = {
+                mappings = {
+                    i = {
+                        ["<esc>"] = actions.close,
+                        ["<C-u>"] = false,
+                    },
+                },
+            }
+        }
+    end,
 }
