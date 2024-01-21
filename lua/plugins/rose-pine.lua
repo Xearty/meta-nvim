10add32f7e38bfb57f7a8e2f5c1b00607acb2876
Xearty <xeartybg@gmail.com>
2024-01-21 07:19:18 +0200

    feat(colorscheme): remove italic

--- lua/plugins/rose-pine.lua
+++ lua/plugins/rose-pine.lua
@@ -4,6 +4,11 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
+        require("rose-pine").setup {
+            styles = {
+                italic = false,
+            },
+        }
         vim.cmd [[colorscheme rose-pine]]
     end,
 }
