982687e41b8a6f732bbdc934ac46624bcd17d168
Xearty <xeartybg@gmail.com>
2024-01-21 20:58:46 +0200

    feat(completion): rounded corners for completion window

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -7,12 +7,23 @@ return {
         "hrsh7th/cmp-path",
         "hrsh7th/cmp-cmdline",
         "hrsh7th/nvim-cmp",
+        "onsails/lspkind.nvim", -- icons
     },
     config = function()
-        -- Set up nvim-cmp.
-        local cmp = require'cmp'
+        local cmp = require("cmp")
 
         cmp.setup({
+            formatting = {
+                format = require("lspkind").cmp_format {
+                    mode = 'symbol', -- show only symbol annotations
+                    maxwidth = 50,   -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+                    -- can also be a function to dynamically calculate max width such as
+                    -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
+                    ellipsis_char = '...',    -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+                    show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+                },
+                expandable_indicator = true,  -- required
+            },
             snippet = {
                 -- REQUIRED - you must specify a snippet engine
                 expand = function(args)
@@ -23,8 +34,8 @@ return {
                 end,
             },
             window = {
-                -- completion = cmp.config.window.bordered(),
-                -- documentation = cmp.config.window.bordered(),
+                completion = cmp.config.window.bordered(),
+                documentation = cmp.config.window.bordered(),
             },
             mapping = cmp.mapping.preset.insert({
                 ['<C-b>'] = cmp.mapping.scroll_docs(-4),
@@ -40,8 +51,8 @@ return {
                 -- { name = 'ultisnips' }, -- For ultisnips users.
                 -- { name = 'snippy' }, -- For snippy users.
             }, {
-                    { name = 'buffer' },
-                })
+                { name = 'buffer' },
+            })
         })
 
         -- Set configuration for specific filetype.
@@ -49,8 +60,8 @@ return {
             sources = cmp.config.sources({
                 { name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
             }, {
-                    { name = 'buffer' },
-                })
+                { name = 'buffer' },
+            })
         })
 
         -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
@@ -67,14 +78,14 @@ return {
             sources = cmp.config.sources({
                 { name = 'path' }
             }, {
-                    { name = 'cmdline' }
-                })
+                { name = 'cmdline' }
+            })
         })
 
+        --[[
         -- Set up lspconfig.
         local capabilities = require('cmp_nvim_lsp').default_capabilities()
         -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-        --[[
         require('lspconfig')['rust_analyzer'].setup {
             capabilities = capabilities
         }
