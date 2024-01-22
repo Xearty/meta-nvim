2d921a1f6082b2de2417f74c91e8c1ae363eed16
Xearty <xeartybg@gmail.com>
2024-01-23 01:25:08 +0200

    refactor: formatting

--- lua/plugins/barbecue.lua
+++ lua/plugins/barbecue.lua
@@ -5,7 +5,7 @@ return {
         "SmiteshP/nvim-navic",
         "nvim-tree/nvim-web-devicons", -- optional dependency
     },
-    after = "nvim-web-devicons", -- keep this if you're using NvChad
+    after = "nvim-web-devicons",       -- keep this if you're using NvChad
     config = function()
         require("barbecue").setup()
     end,
