a0d6376f2d17d75640f23ae30db4d2fc8727e95a
Xearty <xeartybg@gmail.com>
2024-01-24 01:33:24 +0200

    feat(colorscheme): install tokyonight

--- lua/plugins/rose-pine.lua
+++ lua/plugins/rose-pine.lua
@@ -1,14 +1,14 @@
 return {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    lazy = false,
-    priority = 1000,
-    config = function()
-        require("rose-pine").setup {
-            styles = {
-                italic = false,
-            },
-        }
-        vim.cmd [[colorscheme rose-pine]]
-    end,
+    -- "rose-pine/neovim",
+    -- name = "rose-pine",
+    -- lazy = false,
+    -- priority = 1000,
+    -- config = function()
+    --     require("rose-pine").setup {
+    --         styles = {
+    --             italic = false,
+    --         },
+    --     }
+    --     vim.cmd [[colorscheme rose-pine]]
+    -- end,
 }
