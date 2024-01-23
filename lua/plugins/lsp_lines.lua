40f7474c7d6249cab072a7c29cd2e0008986487c
Xearty <xeartybg@gmail.com>
2024-01-24 01:20:07 +0200

    feat(diagnostics): install lsp_lines and uninstall diagflow

---
+++ lua/plugins/lsp_lines.lua
@@ -0,0 +1,16 @@
+return {
+    "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+    keys = {
+        {
+            "<leader>l",
+            function() require("lsp_lines").toggle() end,
+            desc = "Toggle lsp_lines",
+            mode = { "n" },
+        }
+    },
+    config = function()
+        -- Disable virtual_text since it's redundant due to lsp_lines.
+        vim.diagnostic.config({ virtual_text = false })
+        require("lsp_lines").setup()
+    end,
+}
