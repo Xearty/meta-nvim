3afc0d288f456184e4a16f7b4b23935fa4504caf
Xearty <xeartybg@gmail.com>
2024-01-24 23:58:01 +0200

    fix(noice): disable hover and messages for real(using notifier)

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -8,7 +8,7 @@ return {
     config = function()
         require("noice").setup({
             messages = {
-                enable = false,
+                enabled = false,
             },
             lsp = {
                 -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
@@ -16,6 +16,9 @@ return {
                     ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
                     ["vim.lsp.util.stylize_markdown"] = true,
                     ["cmp.entry.get_documentation"] = true,
+                },
+                hover = {
+                    enabled = false,
                 },
             },
             -- you can enable a preset for easier configuration
