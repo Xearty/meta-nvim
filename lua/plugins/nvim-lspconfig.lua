1070a6858908188c571a872d436ec2282b7c035b
Xearty <xeartybg@gmail.com>
2024-01-25 02:06:08 +0200

    feat(telescope): retire fzf-lua in favor of telescope

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -11,12 +11,9 @@ return {
             config = true,
         },
     },
+    event = { "BufReadPost", "BufNewFile" },
     config = function()
         local lspconfig = require("lspconfig")
-
-        vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-            border = "rounded",
-        })
 
         -- Only setup language servers that are not installed through mason
         lspconfig.clangd.setup {}
@@ -40,20 +37,10 @@ return {
                 -- Buffer local mappings.
                 -- See `:help vim.lsp.*` for documentation on any of the below functions
                 local opts = { buffer = ev.buf }
-                vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-                vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
                 vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-                vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
                 vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-                vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-                vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-                vim.keymap.set('n', '<space>wl', function()
-                    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-                end, opts)
-                vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
                 vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
                 vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-                vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
 
                 vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
                 vim.api.nvim_create_autocmd("BufWritePre", {
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,13 +1,70 @@
 return {
-    'nvim-telescope/telescope.nvim',
+    "nvim-telescope/telescope.nvim",
     tag = '0.1.5',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    },
     keys = {
         {
-            "<leader>gb",
-            function()
-                require("telescope.builtin").git_branches()
-            end,
+            "<leader>ff",
+            function() require("telescope.builtin").find_files() end,
+            desc = "Find files",
+        },
+        {
+            "<leader>fc",
+            function() require("telescope.builtin").current_buffer_fuzzy_find() end,
+            desc = "Fuzzy search in current buffer",
+        },
+        {
+            "<leader>fb",
+            function() require("telescope.builtin").git_branches() end,
+            desc = "Find git branches",
+        },
+        {
+            "<leader>fg",
+            function() require("telescope.builtin").live_grep() end,
+            desc = "Live grep",
+        },
+        {
+            "<leader>fs",
+            function() require("telescope.builtin").grep_string() end,
+            desc = "Grep string under cursor",
+        },
+        {
+            "<leader>fq",
+            function() require("telescope.builtin").quickfix() end,
+            desc = "Items in quickfix list",
+        },
+        {
+            "<leader>fd",
+            function() require("telescope.builtin").diagnostics() end,
+            desc = "LSP diagnostics",
+        },
+        {
+            "<leader>fic",
+            function() require("telescope.builtin").lsp_incoming_calls() end,
+            desc = "LSP incoming calls",
+        },
+        {
+            "<leader>foc",
+            function() require("telescope.builtin").lsp_outgoing_calls() end,
+            desc = "LSP outgoing calls",
+        },
+        {
+            "gr",
+            function() require("telescope.builtin").lsp_references() end,
+            desc = "LSP references for word under cursor",
+        },
+        {
+            "gd",
+            function() require("telescope.builtin").lsp_definitions() end,
+            desc = "LSP definitions",
+        },
+        {
+            "gi",
+            function() require("telescope.builtin").lsp_implementations() end,
+            desc = "LSP implementations",
         },
     },
     config = function()
@@ -21,8 +78,18 @@ return {
                         ["<C-u>"] = false,
                     },
                 },
-            }
+            },
+            extensions = {
+                fzf = {
+                    fuzzy = true,                   -- false will only do exact matching
+                    override_generic_sorter = true, -- override the generic sorter
+                    override_file_sorter = true,    -- override the file sorter
+                    case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
+                    -- the default case_mode is "smart_case"
+                },
+            },
         }
-        -- telescope.load_extension("notify") -- needed for noice.nvim
+        telescope.load_extension("fzf")
+        telescope.load_extension("notify") -- needed for noice.nvim
     end,
 }
